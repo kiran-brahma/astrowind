b85c66d8f41fc3ad4f149c43a6ed21fcf8e4dfd7
Kiran Brahma <akbrahma@gmail.com>
2024-06-15 22:28:49 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,9 +36,7 @@ export default defineConfig({
     {webAnalytics: {
       enabled: true,
     },
-    imagesConfig: {
-      sizes: [320, 640, 1280],
-    }
+    
 }),
 
   integrations: [react(),
